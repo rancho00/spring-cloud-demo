358c0c7413589de5b059c2dfd3ae690160ebbfb7
rancho <191405738@qq.com>
2020-03-25 19:26:06 +0800

    添加eureka集群

---
+++ spring-cloud-demo-eureka/spring-cloud-demo-eureka-server-7003/src/main/java/spring/cloud/demo/eureka/server/EurekaServer7003Application.java
@@ -0,0 +1,14 @@
+package spring.cloud.demo.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer7003Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer7003Application.class,args);
+    }
+}
