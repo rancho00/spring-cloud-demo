bb2aa8f0cc1ee3429b64acddaac4b694919c62d3
rancho <191405738@qq.com>
2020-04-03 09:24:06 +0800

    完善eureka

--- spring-cloud-demo-eureka/spring-cloud-demo-eureka-server-7001/src/main/java/spring/cloud/demo/eureka/server/EurekaServer7001Application.java
+++ spring-cloud-demo-eureka/spring-cloud-demo-eureka-server-7001/src/main/java/spring/cloud/demo/eureka/server/EurekaServer7001Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-//启动eureka
+//启动eureka服务端
 @EnableEurekaServer
 public class EurekaServer7001Application {
 
