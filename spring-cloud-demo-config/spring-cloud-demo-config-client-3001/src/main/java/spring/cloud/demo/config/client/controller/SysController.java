c960c1f4d22a234ab734555042ebbf65218e55c3
rancho <191405738@qq.com>
2020-04-07 11:15:01 +0800

    完善config

--- spring-cloud-demo-config/spring-cloud-demo-config-client-3001/src/main/java/spring/cloud/demo/config/client/controller/SysController.java
+++ spring-cloud-demo-config/spring-cloud-demo-config-client-3001/src/main/java/spring/cloud/demo/config/client/controller/SysController.java
@@ -1,11 +1,12 @@
-package spring.cloud.demo.config.client;
+package spring.cloud.demo.config.client.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/sys")
 @RestController
-public class ConfigClientRest {
+public class SysController {
 
     @Value("${spring.application.name}")
     private String name;
