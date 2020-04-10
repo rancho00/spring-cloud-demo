3c3075aa311b57846272bd284cbcdee55422953b
rancho <191405738@qq.com>
2020-04-10 16:51:21 +0800

    客户端动态读取配置属性

--- spring-cloud-demo-config/spring-cloud-demo-config-client-3001/src/main/java/spring/cloud/demo/config/client/ConfigClientApplication.java
+++ spring-cloud-demo-config/spring-cloud-demo-config-client-3001/src/main/java/spring/cloud/demo/config/client/ConfigClientApplication.java
@@ -2,6 +2,8 @@ package spring.cloud.demo.config.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
 
 @SpringBootApplication
 public class ConfigClientApplication {
--- spring-cloud-demo-config/spring-cloud-demo-config-client-3001/src/main/java/spring/cloud/demo/config/client/controller/SysController.java
+++ spring-cloud-demo-config/spring-cloud-demo-config-client-3001/src/main/java/spring/cloud/demo/config/client/controller/SysController.java
@@ -1,11 +1,16 @@
 package spring.cloud.demo.config.client.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/sys")
 @RestController
+/**
+ * 动态刷新
+ */
+@RefreshScope
 public class SysController {
 
     @Value("${spring.application.name}")
