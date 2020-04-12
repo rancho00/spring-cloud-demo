0ebb096ab2efec8fe5c8a22a8fa8add70026ac99
rancho <191405738@qq.com>
2020-04-12 16:54:30 +0800

    新增spring-cloud-demo-sentinel模块

---
+++ spring-cloud-demo-sentinel/spring-cloud-demo-sentinel-provider-8001/src/main/java/spring/cloud/demo/sentinel/provider/SentinelProvider8001Application.java
@@ -0,0 +1,12 @@
+package spring.cloud.demo.sentinel.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelProvider8001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelProvider8001Application.class,args);
+    }
+}
